9ffe7b4ac5f0b97f8ee4d1f642074eea506a87bb
Rasmus Bomholt Rahbek <bomholt.rahbek@gmail.com>
2026-06-09 11:31:34 +0200

    v.0.0.5 monsters Creates

---
+++ monsters.cpp
@@ -0,0 +1,33 @@
+#include "monster.h"
+#include <map>
+#include <iostream>
+
+std::map<std::string, MonsterData> MONSTER_DATA = {
+    {"slime",       {"Slime",  8,  2}},
+    {"goblin",      {"Goblin", 10, 3}},
+    {"skeleton",    {"Skeleton", 12, 4}},
+    {"horse",       {"Horse",  15, 5}},
+    {"Mantis",      {"Mantis", 12, 7}},
+    {"zombie",      {"Zombie",   18, 4}},
+    {"orc",         {"Orc",    20, 7}},
+    {"knight",      {"Knight",   25, 10}},
+    {"giant",       {"Giant",    30, 12}},
+    {"dragon",      {"Dragon", 40, 15}},
+    {"demon",       {"Demon", 50, 20}},
+    {"Demon Lord",  {"Demon Lord", 100, 30}}
+};
+
+Monster::Monster(std::string type) {
+    MonsterData data = MONSTER_DATA[type];
+    name   = data.name;
+    hp     = data.hp;
+    attack = data.attack;
+}
+
+void Monster::print() {
+    std::cout << "Monster: " << name 
+                << " | HP: " << hp 
+                << " | ATK: " << attack << std::endl;
+}
+
+
---
+++ monsters.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <map>
+
+struct MonsterData {
+    std::string name;
+    int hp;
+    int attack;
+};
+
+
+extern std::map<std::string, MonsterData> MONSTER_DATA;
+
+class Monster {
+public:
+    std::string name;
+    int hp;
+    int attack;
+
+    Monster(std::string type);
+    void print();
+};
