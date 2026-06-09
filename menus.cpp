853784ba6b15423f760e0a18f975c8e7eca4eb19
Rasmus Bomholt Rahbek <bomholt.rahbek@gmail.com>
2026-06-09 09:53:45 +0200

    v 0.0.1 Basic framework for the project

---
+++ menus.cpp
@@ -0,0 +1,11 @@
+#include "menus.h"
+
+#include <iostream>
+
+
+void Menu::mainMenu() {
+    std::cout << "Welcome to the Main Menu!" << std::endl;
+    std::cout << "choose your action by entering the according number in your console" << std::endl;
+    std::cout << "1. Start new game"
+    std::cout << "2. "
+}
